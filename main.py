7861b069127b486f0ec43039cf46b92273e7d2d2
Biz0n <192527421+f58dev@users.noreply.github.com>
2026-06-29 20:05:39 +0300

    aig

--- main.py
+++ main.py
@@ -1,43 +1,50 @@
+#import libraries
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 from langchain.agents import create_agent
 
-
 load_dotenv()
 
+#this is fake tool to return the weather
 def get_weather(city: str):
     """Get weather for a given city"""
-    return {'condition':'sunny', 'temperature': 25}
+    return {'condition': 'sunny', 'temperature': 25}
 
+#this is fake tool to return the location
 def get_location():
     """Get user's current location. Use this when the user asks about weather."""
     return "Rome, Italy"
 
-# Initialize Gemini Flash 2.5
+def get_quality(city: str):
+    return {'condition': 'Pollutant'}
+
+# Start Gemini
 llm = ChatGoogleGenerativeAI(
     model="gemini-2.5-flash",
     temperature=0.7,
 )
+#here if you ask the agent what is the weather it will return the get_location
 system_prompt = """
 You are a helpful weather assistant. 
 YOUR WORKFLOW:
 1. If the user asks about weather WITHOUT specifying a location, you MUST:
    - First call get_location() to find their location
    - Then call get_weather(city) with that location
-   
+
 2. If the user provides a city, call get_weather(city) directly.
 
 """
+# the mind
 agent = create_agent(
     model=llm,
     tools=[get_weather, get_location],
     system_prompt=system_prompt
 )
-
+#take the question
 user_query = input("Enter your query: ")
 
 # response1 = llm.invoke("How is the weather in Rome?")
 response1 = agent.invoke(
     {"messages": [{'role': 'user',
-                   'content':user_query}]})
+                   'content': user_query}]})
 print(response1['messages'][-1].content)
